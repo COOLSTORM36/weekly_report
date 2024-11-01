5e77aa3441871d4c7694a05ad3905a96a8cd91c6
COOLSTORM <yoyo.sunshine@hotmail.com>
2024-11-01 15:22:15 +0000

    Update weekly_report.py

--- weekly_report.py
+++ weekly_report.py
@@ -20,20 +20,6 @@ st.title("Hello")
 
 AT_API_KEY = st.secrets["AT_API_KEY"]
 
-# Create a placeholder
-placeholder = st.empty()
-
-while True:
-    # Get the current time
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    # Update the placeholder with the current time
-    placeholder.markdown(f"<h2 style='text-align: center;'>Current UK time: {current_time}</h2>", unsafe_allow_html=True)
-    
-    # Wait for 1 second before updating again
-    time.sleep(1)
-    
-
 
 # ------------------------------------------------------------------------------------------------
 # import airtable API key
