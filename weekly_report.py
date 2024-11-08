837d121313e433658bd84790b8607ce439a61324
COOLSTORM <yoyo.sunshine@hotmail.com>
2024-11-08 14:06:11 +0000

    Update weekly_report.py

--- weekly_report.py
+++ weekly_report.py
@@ -75,9 +75,9 @@ for item in Market_Insight_result:
 # Convert to DataFrame
 market_insight_df = pd.DataFrame(market_insight_flattened_data)
 if 'Attachment' in market_insight_df.columns and market_insight_df['Attachment'].notnull().any():
-    Market_Insight = market_insight_df.drop(columns=['Attachment', 'Link to Details', '领域分类', 'English Summary', 'English Title', '输入人', 'Ready', '记录日期', 'Week', '周报？'])
+    Market_Insight = market_insight_df.drop(columns=['Attachment', 'Link to Details', '领域分类', '中文详情','English Summary', 'English Title', '输入人', 'Ready', '记录日期', 'Week', '周报？'])
 else:
-    Market_Insight = market_insight_df.drop(columns=['Link to Details', '领域分类', 'English Summary', 'English Title', '输入人', 'Ready', '记录日期', 'Week', '周报？'])
+    Market_Insight = market_insight_df.drop(columns=['Link to Details', '领域分类', '中文详情', 'English Summary', 'English Title', '输入人', 'Ready', '记录日期', 'Week', '周报？'])
 
 
 # fetch the data from products doc log
